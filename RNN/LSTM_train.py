18cb75ea7802f2e9b093bdc41cb3432a426c8118
hydrogendeuteride <dumpsterowner@gmail.com>
2024-06-02 16:50:54 +0900

    hyperparameter tuning

--- RNN/LSTM_train.py
+++ RNN/LSTM_train.py
@@ -12,14 +12,14 @@ except ImportError:
     array_library = np
 
 data_line = read_file_to_list('../dataset/tagged_train.txt')
-processed_data_line = reader(data_line[:500])
+processed_data_line = reader(data_line[:3000])
 pos_cnt, word_cnt = count_word_POS(processed_data_line)
 word_to_idx, tag_to_idx = build_vocab(word_cnt, pos_cnt)
 
 x1, y1 = text_to_indices(processed_data_line, word_to_idx, tag_to_idx)
 idx_to_tag = build_reverse_tag_index(tag_to_idx)
 
-model = LSTM(word_dim=len(word_cnt), word_embed_dim=128, tag_dim=len(pos_cnt), hidden_dim=128, max_norm=5, use_gpu=True)
+model = LSTM(word_dim=len(word_cnt), word_embed_dim=8192, tag_dim=len(pos_cnt), hidden_dim=256, max_norm=5, use_gpu=True)
 
 start_time = time.time()
 model.sgd_step(x1[3], y1[3], 0.005)
